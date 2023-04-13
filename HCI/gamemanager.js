05c2e663e09f2ae1f411196a0041af6db4f9bd04
Archit Kumar <57429607+coderepic@users.noreply.github.com>
2023-04-13 10:12:51 -0400

    Upload Project Files

---
+++ HCI/gamemanager.js
@@ -0,0 +1,25 @@
+function initialize(){
+    document.getElementsByClassName("countryButtons").style.display = 'none';
+    document.getElementById("score").style.display = 'none';
+}
+
+function startGame(){
+    document.getElementById("score").style.display = 'block';
+    document.getElementById("playbutton").style.display = 'none';
+    document.getElementById("canadaButton").style.display = 'block';
+    document.getElementById("brazilButton").style.display = 'block';
+    document.getElementById("southAfricaButton").style.display = 'block';
+}
+
+function startCanada(){
+    document.getElementById("brazilButton").style.display = 'none';
+    document.getElementById("southAfricaButton").style.display = 'none';
+}
+
+function CNTower(){
+    
+}
+
+function startCanada(){
+
+}
