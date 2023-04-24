fd085f30a30b36aa36d0f62da9d1a32087be9177
kaigreen4 <kaitlyngreenwell@yahoo.com>
2023-04-24 14:32:47 -0400

    style edited and code updated
    used archits code as it was at 11:30am 4/24. Not final, just committing for reference.

--- HCI/gamemanager.js
+++ HCI/gamemanager.js
@@ -1,39 +1,209 @@
 let score = 0;
+let questionsRemaining = 5;
+let questions = ["cnTowerQuestion", "madagascarQuestion", "tanzaniaQuestion", "indonesiaQuestion", "easterIslandQuestion", "germanyQuestion"];
+let mapQuestions = ["nyMap", "denmarkMap", "thailandMap", "SFMap", "BostonMap", "PNGmap"];
+var questionList;
+currentQuestionIndex = 0;
+var currentSceneType = "titleScreen";
+var gameMode = "landmarks";
+
+//Code below obtained from: 
+//https://www.educative.io/answers/how-to-shuffle-an-array-in-javascript
+function shuffleQuestions(array){
+    array.sort(() => Math.random() - 0.5);
+}
 
 function initialize(){
-    document.getElementsByClassName("countryButtons").style.display = 'none';
+    questionsArray = document.getElementsByClassName("triviaQuestion");
+    document.getElementById("instructions").style.display = 'none';
+    currentSceneType = "titleScreen";
+    document.getElementById("titlePage").style.display = 'block';
+    shuffleQuestions(questions);
+    currentQuestionIndex = 0;
+    questionsRemaining = 5;
+    score = 0;
+    //document.getElementsByClassName("countryButtons").style.display = 'none';
     document.getElementById("scoreGroup").style.display = 'none';
     document.getElementById("scoreVal").innerHTML = "0";
+    document.getElementById("finalScoreDisplay").style.display = 'none';
+    document.getElementById("questionsRemaining").style.display = 'none';
+    document.getElementById("tilePage").style.display = 'block';
+    document.getElementById("topBarContainer").style.display = 'none';
+    for(let i=0; i<questionsArray.length; i++){
+        questionsArray[i].style.display = 'none';        
+    }
 }
 
-function startGame(){
+function displayInstructions(){
+    currentSceneType = "instructionsScreen";
+    document.getElementById("instructions").style.display = 'block';
+    document.getElementById("titlePage").style.display = 'none';
+}
+
+function startGame(type){
+    currentSceneType = "pickGameMode";
+    if(type==1){
+        questionList = questions;
+    }
+    else{
+        questionList = mapQuestions;
+    }
+    shuffleQuestions(questionList);
+    currentQuestionIndex = 0;
+    questionsRemaining = 5;
+    score = 0;
+    // document.getElementById("instructions").style.display = 'none';
+    // document.getElementById("canadaButton").style.display = 'block';
+    // document.getElementById("brazilButton").style.display = 'block';
+    // document.getElementById("finalScoreDisplay").style.display = 'none';
+    // document.getElementById("scoreGroup").style.display = 'none';
+    // document.getElementById("scoreVal").innerHTML = score.toString();
+    // document.getElementById("questionsRemaining").style.display = 'none';
+}
+
+function startTrivia(type){
+    currentSceneType = "question";
+    if(type==1){
+        questionList = questions;
+        gameMode = "landmarks";
+    }
+    else{
+        questionList = mapQuestions;
+        gameMode = "maps";
+    }
+    questionsRemaining = 5;
+    currentQuestionIndex = 0;
+    score = 0;
+    shuffleQuestions(questionList);
+    document.getElementById("instructions").style.display = 'none';
     document.getElementById("scoreGroup").style.display = 'block';
     document.getElementById("scoreVal").innerHTML = score.toString();
-    document.getElementById("selectCountry").style.display = 'block';
-    document.getElementById("playbutton").style.display = 'none';
-    document.getElementById("canadaButton").style.display = 'block';
-    document.getElementById("brazilButton").style.display = 'block';
-    document.getElementById("southAfricaButton").style.display = 'block';
-}
-
-function startCanada(){
-    document.getElementById("brazilButton").style.display = 'none';
-    document.getElementById("canadaButton").style.display = 'none';
-    document.getElementById("cnTowerQuestion").style.display = 'block';
-    document.getElementById("selectCountry").style.display = 'none';
+    document.getElementById("questionsRemaining").style.display = 'block';
+    document.getElementById("questionsRemainingElement").innerHTML = questionsRemaining.toString();
+    document.getElementById("questionsAndScore").style.display = 'block';
+    loadQuestion();
 }
 
 function resultScreen(correct){
-    document.getElementById("cnTowerQuestion").style.display = "none";
+    // clearInterval(timerId);
+    // document.getElementById("cnTowerQuestion").style.display = "none";
+    resetTimer();
+    optionsArray = document.getElementsByClassName("optionsContainer");
+    questionsArray = document.getElementsByClassName("triviaQuestion");
+    questionTextArray = document.getElementsByClassName("questionText");
+        
+    // for(let i=0; i<questionsArray.length; i++){
+    //     questionsArray[i].style.display = 'none';        
+    // }
+
+    for(let i=0; i<optionsArray.length; i++) {
+        optionsArray[i].style.display = 'none';
+    }
 
     if (correct == 1) {
         score = score + 1;
         document.getElementById("correctDisplay").style.display = "block";
+        //document.getElementById("questionText").innerHTML = "Correct!";  
+
+        for(let i=0; i<questionTextArray.length; i++){
+            questionTextArray[i].innerHTML = "Correct!";
+        }
     }
     else {
         document.getElementById("incorrectDisplay").style.display = "block";
+        //document.getElementById("questionText").innerHTML = "Incorrect!";  
+        for(let i=0; i<questionTextArray.length; i++){
+            questionTextArray[i].innerHTML = "Incorrect!";
+        }
     }   
-
+    questionsRemaining = questionsRemaining - 1;
     document.getElementById("scoreVal").innerHTML = score.toString();
+    document.getElementById("questionsRemainingElement").innerHTML = questionsRemaining.toString();
+    currentSceneType = "results";
+}
+
+function displayResults(){
+    document.getElementById("finalScoreDisplay").style.display = "block";        
+}
+
+function loadQuestion(){
+    //document.getElementById("questionText").innerHTML = "What is this geographic landmark?";  
+    questionsArray = document.getElementsByClassName("triviaQuestion");
+    questionTextArray = document.getElementsByClassName("questionText");
+    document.getElementById("timerDiv").style.display = 'block';
+
+
+    if(gameMode == "landmarks"){
+        for(let i=0; i<questionTextArray.length; i++){
+            questionTextArray[i].innerHTML = "What is this geographic landmark?";
+        }
+    }
+    else{
+        for(let i=0; i<questionTextArray.length; i++){
+            questionTextArray[i].innerHTML = "What location is depicted in the map?";
+        }
+    }
+
+    for(let i=0; i<questionsArray.length; i++){
+        questionsArray[i].style.display = 'none';        
+    }
+
+    startTimer();
+    if(questionsRemaining <= 0){
+        currentQuestionIndex = 0;
+        resetTimer();
+        displayResults();
+    }
+    else{
+        currentQuestionIndex++;
+        document.getElementById(questionList[currentQuestionIndex]).style.display = "block";
+        document.getElementById(questionList[currentQuestionIndex] + "Options").style.display = "flex";
+    }
+    document.getElementById("correctDisplay").style.display = "none";
+    document.getElementById("incorrectDisplay").style.display = "none";
+    document.getElementById("questionsRemaining").style.display = "block";
 
 }
+
+let timerId = null;
+let secondsLeft = 30;
+function startTimer() {    
+    const timerElement = document.getElementById("timer");
+    timerElement.style.display = "block";
+    timerId = setInterval(() => {
+        secondsLeft--;
+        if (secondsLeft < 0) {
+            resultScreen(0);
+            clearInterval(timerId);
+            secondsLeft = 30;
+        } 
+        else {
+        timerElement.innerHTML = formatTime(secondsLeft);
+        }
+    }, 1000);
+}
+
+function resetTimer() {
+    console.log('resetting timer');
+    clearInterval(timerId);
+    secondsLeft = 30;
+    document.getElementById("timer").innerHTML = formatTime(secondsLeft);
+    document.getElementById("timer").style.display = "none";         
+}
+
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${padZeroes(minutes)}:${padZeroes(remainingSeconds)}`;
+}
+
+function padZeroes(value) {
+    return value < 10 ? `0${value}` : value;
+}
+
+
+
+// function update_test_value(){
+//     let position = positionCheck();
+//     document.getElementById(inputtest).innerHTML = position.toString();
+// }
--- HCI/main.js
+++ HCI/main.js
@@ -14,6 +14,105 @@ var frames = {
         frames.socket = new WebSocket(url);
         frames.socket.onmessage = function (event) {
             frames.show(JSON.parse(event.data));
+            //My own line below:
+            let frame = JSON.parse(event.data);
+            if(frame.people.length > 0){
+                document.getElementById("inputtest").innerHTML = "Someone at display";
+                document.getElementById("postest").innerHTML = frame.people[0].joints[8].position.x.toString();
+                let lh = frame.people[0].joints[8].position.y;
+                let ls = frame.people[0].joints[5].position.y;
+                let rh = frame.people[0].joints[15].position.y;
+                let rs = frame.people[0].joints[12].position.y;
+
+                let lhx = frame.people[0].joints[8].position.x;
+                let lsx = frame.people[0].joints[5].position.x;
+                let rhx = frame.people[0].joints[15].position.x;
+                let rsx = frame.people[0].joints[12].position.x;
+                
+                if(leftArmSelection(lhx, lsx)){
+                    //Select left option
+                    //resultsScreen(1);
+                    document.getElementById("selectionTEster").innerHTML = "left selection";
+                }
+                else if(rightArmSelection(rhx, rsx)){
+                    //Select right option
+                    //resultsScreen(0);
+                    document.getElementById("selectionTester").innerHTML = "right selection";
+                }
+                else{
+                    document.getElementById("selectionTester").innerHTML = "no selection";
+                }
+
+                console.log(lhx.toString());
+                console.log(lsx.toString());
+
+                //Allow user to select answers with their hands
+                if(currentSceneType == "question"){
+                    if(leftArmSelection(lhx, lsx) && rightArmSelection(rhx, rsx)){
+                        //Nothing happens
+                    }
+                    else if(leftArmSelection(lhx, lsx)){
+                        //Select left option
+                        resultsScreen(1);
+                    }
+                    else if(rightArmSelection(rhx, rsx)){
+                        //Select right option
+                        resultsScreen(0);
+                    }
+                    else{
+                        //nothing happens;
+                    }
+                }
+
+                if(currentSceneType == "instructionsScreen"){
+                    if(leftArmSelection(lhx, lsx) && rightArmSelection(rhx, rsx)){
+                        //Nothing happens
+                    }
+                    else if(leftArmSelection(lhx, lsx)){
+                        //Select left option
+                        console.log("left");
+                        startTrivia(1);
+                    }
+                    else if(rightArmSelection(rhx, rsx)){
+                        //Select right option
+                        console.log("right");
+                        startTrivia(2);
+                    }
+                }
+
+                //Allow user to transition with their arms.
+                if(currentSceneType == "titleScreen"){
+                    if(armsRaised(lh, ls, rh, rs) == "left"|| armsRaised(lh, ls, rh, rs) == "right"){
+                        displayInstructions();
+                    }
+                }
+                else if(currentSceneType == "instructionsScreen"){
+                    if(leftArmRaised(lh, ls)){
+                        startGame();
+                    }
+                }
+                else if(currentSceneType == "pickGameMode"){
+                    if(leftArmSelection(lhx, lsx)){
+                        startTrivia(0);
+                    }
+                    else if(rightArmSelection(rhx, rsx)){
+                        startTrivia(1);
+                    }
+                }
+                else if(currentSceneType == "question"){
+                    if(leftArmSelection(lhx, lsx)){
+                        resultScreen(0);
+                    }
+                    else if(rightArmSelection(rhx, rsx)){
+                        resultScreen(1);
+                    }
+                }
+            }
+            else{
+                //initialize();
+                document.getElementById("inputtest").innerHTML = "Nobody at display";
+                document.getElementById("postest").innerHTML = 'no people';
+            }
         }
     },
 
@@ -27,7 +126,7 @@ var twod = {
 
     // create a connection to the camera feed
     start: function () {
-        var url = "ws://" + host + "/twod";
+        var url = "ws://" + host + "/frames";
         twod.socket = new WebSocket(url);
 
         // whenever a new frame is received...
@@ -43,3 +142,85 @@ var twod = {
         $('img.twod').attr("src", 'data:image/pnjpegg;base64,' + twod.src);
     },
 };
+
+//EDITED CODE
+function sendPositionToDisplay(){
+    let position = positionCheck();
+    document.getElementById("inputtest").innerHTML = position.toString();
+}
+
+function leftArmRaised(lh, ls){
+    if(lh < ls){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function rightArmRaised(rh, rs){
+    if(rh < rs){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function leftArmSelection(lhx, lsx){
+    if((lhx - lsx) > 450){
+        return true;
+    }
+    else{
+        return false
+    }
+}
+
+function rightArmSelection(rhx, rsx){
+    if((rsx - rhx) > 450){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function isPlayerPresent(frameinput){
+    if(frameinput.people.length > 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function displayPosition(frame){
+    if(positionCheck(frame) == "left"){
+        document.getElementById("postest").innerHTML = "left";
+    }
+    else if(positionCheck(frame) == "right"){
+        document.getElementById("postest").innerHTML = "right";
+    }
+    else{
+        document.getElementById("postest").innerHTML = "nobody here!";
+    }
+}
+
+function armsRaised(lh, ls, rh, rs){
+    if(leftArmRaised(lh, ls) && rightArmRaised(rh, rs)){
+        document.getElementById("armtest").innerHTML = "both arms raised";
+        return "both";
+    }
+    else if(leftArmRaised(lh, ls)){
+        document.getElementById("armtest").innerHTML = "left arm raised";
+        return "left";
+    }
+    else if(rightArmRaised(rh, rs)){
+        document.getElementById("armtest").innerHTML = "right arm raised";
+        return "right";
+    }
+    else{
+        document.getElementById("armtest").innerHTML = "no arms raised";
+        return "none";
+    }
+}
